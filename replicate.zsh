6a77fe1352c33633978a8e31de87cd1acb1c46d7
Leo Allen <leopallen@gmail.com>
2024-05-22 18:44:03 +0200

    adding scaffolding of replication package

---
+++ replicate.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+# BE SURE TO ACTIVATE THE SHELL SCRIPT USING:
+# chmod +x replicate.zsh
+
+# RUN WITH:
+# ./replicate.zsh
+
+# Define the image name
+IMAGE_NAME="simple_analysis_app"
+
+# Generate the data.csv file
+echo "Generating data.csv..."
+# add your data generation proceedure here, or skip
+
+# Build the Docker image (drop --no-cache once done developing)
+echo "Building the Docker image: {$IMAGE_NAME}"
+docker build --no-cache -t $IMAGE_NAME .
+
+# Run the Docker container
+echo "Running the Docker container..."
+docker run --rm -v $(pwd)/results:/results $IMAGE_NAME
+
+# Notify completion
+echo "Container run completed. Check results/* for files"
---
+++ src/main.py
@@ -0,0 +1,37 @@
+# src/main.py
+
+import pandas as pd
+import statsmodels.api as sm
+
+# replace this with your data analysis / import relevant functions
+def do_simple_analysis():
+    # Read a CSV file with data 
+    # (in practice, import more complex data generation / regression / artifact genetion procedures)
+    data_df = pd.read_csv('data/weather.csv')
+
+    # Prepare the data for OLS regression - picked these arbitrarily :)
+    arbitrary_regressors = [
+        'Humidity',
+        'Wind Speed (km/h)',
+        'Wind Bearing (degrees)',
+        'Visibility (km)',
+        'Pressure (millibars)'
+    ]
+
+    X = data_df[arbitrary_regressors]
+    y = data_df['Apparent Temperature (C)']
+    X = sm.add_constant(X)  # Adds a constant term to the predictor
+
+    # Fit the model
+    model = sm.OLS(y, X).fit()
+
+    # Get the summary
+    summary = model.summary()
+
+    return summary
+
+# Save the summary to a .tex file
+if __name__ == '__main__':
+    with open('/results/results.tex', 'w') as f:
+        summary = do_simple_analysis()
+        f.write(summary.as_latex())
