cf15b4f3a9bca85feacb7f3e5fcabc81c10ecd2c
Leo Allen <leopallen@gmail.com>
2024-06-21 12:49:39 +0200

    Removing commented-out  code from replicate.zsh script

--- replicate.zsh
+++ replicate.zsh
@@ -9,24 +9,3 @@
 docker-compose up --build --remove-orphans
 docker-compose down --rmi all
 
-
-# Define the image name
-# IMAGE_NAME="simple_analysis_app"
-
-# # Generate the data.csv file
-# echo "Generating data.csv..."
-# # add your data generation proceedure here, or skip
-
-# # Build the Docker image (drop --no-cache once done developing)
-# echo "Building the Docker image: {$IMAGE_NAME}"
-# docker build --no-cache -t $IMAGE_NAME .
-
-# # Run the Docker container
-# echo "Running the Docker container..."
-# docker run --rm -v $(pwd)/results:/results $IMAGE_NAME
-
-# # Notify completion
-# echo "Container run completed. Check results/* for files"
-
-# # Tear down image to save space
-# docker rmi $IMAGE_NAME
